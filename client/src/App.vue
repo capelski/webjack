8875b6dd4047116f80015870e7712a2d54ea116b
Carles Capellas <capellas.carles@gmail.com>
2019-05-27 11:25:42 +0200

    Extract menu into separate component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,63 +1,34 @@
 <template>
     <div id="app" class="full-height">
-        <div class="full-height">
-            <!-- TODO Create a Home component -->
-            <div class="main-menu full-height centered" v-if="!gameMode && !remoteLoading">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 text-center">
-                            <p class="title">Webjack</p>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-xs-12 text-center">
-                            <p class="subtitle">Cashless multiplayer blackjack</p>
-                        </div>
-                    </div>
-
-                    <div class="row top-space-20">
-                        <div class="col-xs-12 text-center">
-                            <button type="button" class="btn btn-primary" v-on:click="joinOfflineTable">
-                                Play offline
-                            </button>
-                            <button type="button" class="btn btn-success" v-on:click="joinOnlineTable">
-                                Play online
-                            </button>
-                            <button type="button" class="btn btn-warning" v-on:click="joinBasicStrategyTable">
-                                Practice basic strategy
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <BasicStrategyTable
-                :renderCondition="gameMode === GameModes.basicStrategy"
-                v-on:TableExited="exitTable"
-            />
-            <LocalTable
-                :renderCondition="gameMode === GameModes.local"
-                v-on:TableExited="exitTable"
-            />
-            <RemoteTable
-                :serverUrl="serverUrl"
-                :renderCondition="gameMode === GameModes.remote"
-                v-on:LoadingStarted="setLoading"
-                v-on:LoadingFinished="unsetLoading"
-                v-on:TableJoined="joinOnlineTable"
-                v-on:TableExited="exitTable"
-            />
-        </div>
+        <Menu v-if="!gameMode && !remoteLoading"/>
+        <BasicStrategyTable
+            :renderCondition="gameMode === GameModes.basicStrategy"
+            v-on:TableExited="exitTable"
+        />
+        <LocalTable
+            :renderCondition="gameMode === GameModes.local"
+            v-on:TableExited="exitTable"
+        />
+        <RemoteTable
+            :serverUrl="serverUrl"
+            :renderCondition="gameMode === GameModes.remote"
+            v-on:LoadingStarted="setLoading"
+            v-on:LoadingFinished="unsetLoading"
+            v-on:TableJoined="joinOnlineTable"
+            v-on:TableExited="exitTable"
+        />
     </div>
 </template>
 
 <script lang="ts">
     import { RemoteTable, BasicStrategyTable, LocalTable } from 'webjack-ui-components';
     import { GameModes } from './game-modes';
+    import Menu from './Menu.vue';
 
     export default {
         components: {
             BasicStrategyTable,
+            Menu,
             LocalTable,
             RemoteTable
         },
@@ -78,12 +49,6 @@
         methods: {
             exitTable()  {
                 this.$store.dispatch('exitTable');
-            },
-            joinBasicStrategyTable() {
-                this.$store.dispatch('joinBasicStrategyTable');
-            },
-            joinOfflineTable() {
-                this.$store.dispatch('joinOfflineTable');
             },
             joinOnlineTable() {
                 this.$store.dispatch('joinOnlineTable');
@@ -123,17 +88,5 @@
 
     .top-space-20 {
         margin-top: 20px;
-    }
-
-    .main-menu .title {
-        font-size: 75px;
-    }
-
-    .main-menu .subtitle {
-        font-size: 20px;
-    }
-
-    .main-menu .btn {
-        margin-top: 10px;
     }
 </style>
---
+++ client/src/Menu.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="full-height centered main-menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <p class="title">Webjack</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <p class="subtitle">Cashless multiplayer blackjack</p>
+                </div>
+            </div>
+
+            <div class="row top-space-20">
+                <div class="col-xs-12 text-center">
+                    <button type="button" class="btn btn-primary" v-on:click="joinOfflineTable">
+                        Play offline
+                    </button>
+                    <button type="button" class="btn btn-success" v-on:click="joinOnlineTable">
+                        Play online
+                    </button>
+                    <button type="button" class="btn btn-warning" v-on:click="joinBasicStrategyTable">
+                        Practice basic strategy
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Menu',
+        methods: {
+            joinBasicStrategyTable() {
+                this.$store.dispatch('joinBasicStrategyTable');
+            },
+            joinOfflineTable() {
+                this.$store.dispatch('joinOfflineTable');
+            },
+            joinOnlineTable() {
+                this.$store.dispatch('joinOnlineTable');
+            }
+        }
+    }
+</script>
+
+<style>
+    .main-menu .title {
+        font-size: 75px;
+    }
+
+    .main-menu .subtitle {
+        font-size: 20px;
+    }
+
+    .main-menu .btn {
+        margin-top: 10px;
+    }
+</style>
