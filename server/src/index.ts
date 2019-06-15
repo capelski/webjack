7c3853b2d312bb29802df33f8081058c6e4d3190
Carles Capellas <capellas.carles@gmail.com>
2019-06-15 16:38:00 +0200

    Modify dependencies versions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,6 @@
 import getExpressApp from './get-express-app';
 
 const app = getExpressApp();
-app.listen(3000, (error: any) => {
-	if (error) {
-		console.error(error);
-	}
-	else {
-		console.log('Express server listening on port 3000');
-	}
+app.listen(3000, () => {
+	console.log('Express server listening on port 3000');
 });
